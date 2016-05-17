9fd10e185f740b690d0c7554b27d3334987ca52b
Karl <karlren@shixi.cn>
2016-05-17 10:57:08 +0800

    创建项目

---
+++ GoogleMapsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleMapsDemo
+//
+//  Created by Karl on 16/5/17.
+//  Copyright © 2016年 Karl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoogleMapsDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoogleMapsDemo
+//
+//  Created by Karl on 16/5/17.
+//  Copyright © 2016年 Karl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
